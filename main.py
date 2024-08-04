db14da46ca094f4b8b2dc2066ba5b6143b0b2354
Indu <indulahari6@gmail.com>
2024-08-04 13:04:18 +0530

    Getting the user data (POST Method)

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        middle_name = request.form["middle_name"]
+        last_name = request.form["last_name "]
+        email = request.form["email"]
+        mobile = request.form["mobile"]
+        date = request.form["date"]
+        occupation = request.form["occupation"]
     return render_template("index.html")
 
 
