215f0e53432940db41dd71f8da7d3d9ca12d668e
Indu <indulahari6@gmail.com>
2024-08-07 09:33:39 +0530

    Create a database

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from flask import Flask, render_template, request
+# this library allows to interact with database using more high-level code, makes easier to manage database and data
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Parameters of Database
+app.config["SECRET_KEY"] = "myapplication123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+# Instantiate Database with flask instance 'app' as argument
+db = SQLAlchemy(app)
+
+
+# To create a database we need database model , we create it by using class
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    middle_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    mobile = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -16,4 +36,7 @@ def index():
     return render_template("index.html")
 
 
-app.run(debug=True, port=5001)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5001)
